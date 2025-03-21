b955d91f0b9e3ae7555d8c674d2c65b583daab0f
Syed Sajjad Hussain <syedsajjadhussain014@gmail.com>
2025-03-22 01:48:34 +0530

    Fixed build issue

--- src/components/experienceSection/ExperienceTopRight.jsx
+++ src/components/experienceSection/ExperienceTopRight.jsx
@@ -1,18 +1,7 @@
 const ExperienceTopRight = () => {
   return (
     <div className="xl:w-[25%] lg:w-[30%] bg-gradient-to-br from-gray-800 to-gray-900 p-8 rounded-2xl shadow-2xl">
-      <p className="text-lg text-gray-400">
-        I specialize in{" "}
-        <span className="font-bold text-orange-500">
-          React and modern JavaScript
-        </span>
-        , leveraging best practices to create scalable and maintainable
-        solutions. <br />
-        My experience spans working on diverse projects, from small business
-        websites to{" "}
-        <span className="font-bold text-cyan-500">complex front-end systems</span>,
-        always aiming for clean code and exceptional user experiences.
-      </p>
+<p className="text-lg text-gray-400"> I love to{" "} <span className="font-bold text-orange-500"> build projects using Next.js </span> , and I feel truly excited to design and develop solutions that enhance user experiences. <br /> My passion lies in crafting everything from{" "} <span className="font-bold text-cyan-500">simple, elegant interfaces</span> to{" "} <span className="font-bold text-cyan-500">dynamic, full-stack applications</span>, always focusing on creating intuitive and engaging experiences for users. </p>
     </div>
   );
 };
